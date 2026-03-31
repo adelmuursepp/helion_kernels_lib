ebc74cefe9de0a00755d2242731d4613274fa365
Adel Muursepp <adel.muursepp@gmail.com>
2026-03-31 15:20:02 -0400

    Adding matmuls to swigly pytorch files

--- swiglu/swiglu_pytorch.py
+++ swiglu/swiglu_pytorch.py
@@ -2,5 +2,8 @@ import torch
 import torch.nn.functional as F
 
 def swiglu_pytorch(x1, x2):
-    return F.silu(x1) * x2
+    both = F.linear(x, W)
+    gate = both[:, :hidden_dim]
+    up = both[:, hidden_dim:]
+    return F.silu(gate) * up
     
--- swiglu/swiglu_pytorch_compile.py
+++ swiglu/swiglu_pytorch_compile.py
@@ -2,5 +2,14 @@ import torch
 import torch.nn.functional as F
 
 @torch.compile
-def swiglu_pytorch_compile(x1, x2):
-    return F.silu(x1) * x2
+def swiglu_pytorch_compile_stacked(x1, W):
+    both = F.linear(x, W)
+    gate = both[:, :both.shape[1]//2]
+    up = both[:, both.shape[1]//2:]
+    return F.silu(gate) * up
+
+@torch.compile
+def swiglu_pytorch_compile_separate(x1, W1, W2):
+    gate = F.linear(x, W1)
+    up = F.linear(x, W2)
+    return F.silu(gate) * up
