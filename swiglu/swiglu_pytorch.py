a37445d5c796a6df9ecb6c1ba1d2797a1b5fd065
Adel Müürsepp <adel.muursepp@gmail.com>
2026-04-01 12:00:01 -0400

    Add eager pytorch swiglu

--- swiglu/swiglu_pytorch.py
+++ swiglu/swiglu_pytorch.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn.functional as F
 
-def swiglu_pytorch(x1, x2):
-    both = F.linear(x, W)
-    gate = both[:, :hidden_dim]
-    up = both[:, hidden_dim:]
+def swiglu_pytorch(x, w1, w2):
+    gate = F.linear(x, w1)
+    up = F.linear(x, w2)
     return F.silu(gate) * up
     
